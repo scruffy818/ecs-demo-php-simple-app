32af25283f629f107ade8927791a58b960ce160d
scruffy818 <joshuadhart@hotmail.com>
2018-09-30 13:59:42 -0700

    Update index.php

--- src/index.php
+++ src/index.php
@@ -12,6 +12,7 @@
         <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     </head>
 
+    
     <body>
         <div class="container">
             <div class="hero-unit">
